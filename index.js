3251e2f79e22d71c8545bfe2041b6a141396fbae
Mohamed Magoor <mohamedmagoor6@gmail.com>
2023-12-04 02:44:34 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var map = L.map("map").setView([40.712776, -74.005974], 8);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  maxZoom: 18,
+}).addTo(map);
